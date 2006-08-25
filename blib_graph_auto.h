5ec6dd632700307db29b78776cf4b6a91ca4df57
crb002 <crb002@d6b4f4f2-491a-0410-86bc-4f37dadf027e>
2006-08-25 03:17:23 +0000

    Well I screwed it up adding the auto code (which dies 20 minutes in) but might as well have a backup

--- blib_graph_auto.h
+++ blib_graph_auto.h
@@ -6,6 +6,9 @@
 #include "blib_cell_stack.h"
 #include "blib_schreier.h"
 #include "blib_error.h"
+
+
+
 /*
 	Using the compressed partiton representation
 	Each partition is denoted by a increasing sequence of numbers
@@ -73,6 +76,10 @@ blib_graph_auto_storage* blib_graph_auto_init( blib_graph* g, blib_schreier* sch
 	}
 	return stuff;
 }
+
+
+
+
 
 void blib_graph_auto_reset( blib_graph* g, blib_schreier* schreier,int* orbits, blib_partition* pre_part,blib_graph_auto_storage* stuff){
 	int i,size;
@@ -177,12 +184,19 @@ void blib_graph_auto_split_cells(blib_graph_auto_storage* stuff,int depth, int s
 			}	
 	}
 	blib_partition_split_by_key(stuff->part_stack[depth],stuff->adj_arr,stuff->dirty_cell_arr,&stuff->dirty_cells);
+	if(stuff->part_stack[depth]->cell_count > stuff->part_stack[depth]->size){
+		
+		BLIB_ERROR(" ");	
+	}
+	
+	
 } 
 
 void blib_graph_auto_part_refine(blib_graph_auto_storage* stuff, int depth)
 {
-	int i,j,split_cell_size,db0,db1;
+	int i,j,split_cell_size,db0,db1,db3,db4;
 	/*Push cells onto stack*/
+	
 	db1=stuff->split_stack->cells_allocated;
 	/*blib_error_tabs(depth);fprintf(stderr,"auto_part_refine(%d)BEFORE",depth);*/
 	/*blib_error_tabs(depth);blib_partition_print(stuff->part_stack[depth],stderr);*/
@@ -190,23 +204,41 @@ void blib_graph_auto_part_refine(blib_graph_auto_storage* stuff, int depth)
 		blib_partition_get_cell(stuff->part_stack[depth], i-1, stuff->scratch_arr, &split_cell_size);
 		stuff->split_stack=blib_cell_stack_push(stuff->split_stack,stuff->scratch_arr, split_cell_size);
 	}
+	db3=stuff->part_stack[depth]->cell_count;
 	/*Pop top element, split cells on it, till stack is empty  */
 	db0=stuff->split_stack->cells_allocated;
+	db4=0;
 	while(blib_cell_stack_depth(stuff->split_stack)>0){
+		
 		blib_cell_stack_pop(stuff->split_stack, stuff->split_cell, &split_cell_size);
 		/*Put the cell to split on in stuff->split_cell*/
 		blib_graph_auto_split_cells(stuff,depth,split_cell_size);
+		
+		
+		
+		if(stuff->part_stack[depth]->cell_count >  stuff->part_stack[depth]->size ){
+			BLIB_ERROR(" ");
+			blib_partition_print(stuff->part_stack[depth],stderr);
+			i=1;
+		}
+		
+		
 		for(i=stuff->dirty_cells-1;i>=0;i--){
 			j=split_cell_size;
 			blib_partition_get_cell_at(stuff->part_stack[depth],stuff->dirty_cell_arr[i],
 									   stuff->scratch_arr,&split_cell_size);
 			stuff->split_stack=blib_cell_stack_push(stuff->split_stack, stuff->scratch_arr,split_cell_size);
 		}
+		
+		
+		db4++;
 	}
+	
+
 	/*
 	blib_error_tabs(depth);fprintf(stderr,"auto_part_refine(%d)AFTER",depth);
-	blib_error_tabs(depth);blib_partition_print(stuff->part_stack[depth],stderr);*/
-
+	 blib_error_tabs(depth);blib_partition_print(stuff->part_stack[depth],stderr);*/
+	
 }
 
 
@@ -303,6 +335,21 @@ int blib_graph_auto_sub(blib_graph_auto_storage* stuff, int depth)
 	else
 		best_flag=0;
 	cells=blib_partition_cell_count(stuff->part_stack[depth]);
+	if(cells>blib_graph_size(stuff->graph)){
+		blib_partition_print(stuff->part_stack[depth],stderr);
+		fprintf(stderr,"\n# ");
+		for(i=0;i<blib_graph_size(stuff->graph);i++){
+			if( BLIB_DEBUG_ARR[i] >=0){
+				fprintf(stderr," %d ",BLIB_DEBUG_ARR[i]);
+			}
+			else{
+				break;
+			}
+		}
+		fprintf(stderr,"# (%d) \n", BLIB_DEBUG_X);
+		BLIB_ERROR(" ");	
+	}
+	
 	result=blib_graph_auto_part_test(stuff,depth);
 	/*blib_error_tabs(depth);BLIB_ERROR("result was %d",result);*/
 	/*Worse off so backtrack*/
@@ -349,7 +396,7 @@ int blib_graph_auto_sub(blib_graph_auto_storage* stuff, int depth)
 			best_flag=1;
 		if(result == 0){ /*We got an automorph, so backtrack up to the parent of the best found partition*/
 			if(best_flag==0){
-				blib_error_tabs(depth);BLIB_ERROR("returning 0");
+				/*blib_error_tabs(depth);BLIB_ERROR("returning 0");*/
 				return 0;
 			}
 		}
@@ -381,13 +428,37 @@ void blib_graph_auto(blib_graph* graph, int* orbits, int* certificate, blib_schr
 
 void blib_graph_auto_persistent(blib_graph* graph, int* orbits, int* certificate, blib_schreier* schreier,blib_partition* pre_part,blib_graph_auto_storage* stuff)
 {
+	int d1,d2;
 	blib_graph_auto_reset(graph,schreier,orbits,pre_part,stuff);
+	d1=stuff->part_stack[0]->cell_count;
+	/*BLIB_ERROR("Printing the stack before it gets farked");
+	blib_partition_print(stuff->part_stack[0],stderr);*/
+	
 	blib_graph_auto_part_refine(stuff,0);
+	/*BLIB_ERROR("Printing the stack after it gets farked");
+	blib_partition_print(stuff->part_stack[0],stderr);*/
+	d2=stuff->part_stack[0]->cell_count;
 	blib_graph_auto_sub(stuff,0);
 	blib_graph_auto_finalize_persistent(stuff,graph,schreier,orbits,certificate);
 }
 
-
-
+void blib_graph_auto_edge_colored(blib_graph* g, int* orbs){
+	blib_graph* g2;
+	int i,j,edge_count;
+/*	edge_count=blib_graph_edge_num(g);*/
+	
+	/*g2=blib_graph_allocate(blib_graph_size(g)+edge_count);*/
+	/* add edges to g*/
+	/* Partition verts off from edges*/
+	/*partition vert color classes*/
+	/*partition edge color classes*/
+	/*Run graph auto*/
+	/*return the orbits of the vertex verticies as normal*/
+		
+	
+	
+	
+	
+}
 
 #endif /*_BLIB_GRAPH_AUTO_DEF*/

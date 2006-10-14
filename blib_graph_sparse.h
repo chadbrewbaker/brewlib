e8581b100d73c0f9ace8ab2ad7efcd32a528e122
crb002 <crb002@d6b4f4f2-491a-0410-86bc-4f37dadf027e>
2006-10-14 06:13:48 +0000

    Added sparse matrix header,and I think it now works OK

--- blib_graph_sparse.h
+++ blib_graph_sparse.h
@@ -3,51 +3,33 @@
 #include "blib_error.h"
 
 #define BLIB_GRAPH_SPARSE_MIN_ALLOC 5
-#define BLIB_GRAPH_SPARSE_GROWTH_FACTOR 1.333
+#define BLIB_GRAPH_SPARSE_GROWTH_FACTOR 1.3
 #define BLIB_GRAPH_SPARSE_NO_EDGE_VAL 0
 
-
-/*
-  Ok what do we do with edge and vertex values? Do we want to store adornments? Heck why not. We can always make a stripped version. Question on how to store them....
- 
- 1. Tack onto edges as a two valued array (dest,val)
- 2. Store as a sperate array
- 
- 
- */
-
 /*Space efficency!!!*/
-
-tyepedef struct blib_graph_sparse_edge_t{
-	int dest;
-	int val;
-} blib_graph_sparse_edge;
-
+/*Store lists of vertices, no fancypants linked lists*/
 typedef struct blib_graph_sparse_t{
-	int size,edges;
+	int size;
 	int* alloced_edges;
 	int* used_edges;
-	/*want to make storage generic, but for now hardcode and make general accesors*/
-	blib_graph_sparse_edge **adj;
-	/*For any edge iterators. We kind of need these to get linear time traversal of sparse graphs*/
-	int itr_src,itr_dest,itr_index;
-	
+	int** adj;
+	int* buffer;
+	int buffer_size;
 }blib_graph_sparse;
 
 
 blib_graph_sparse* blib_graph_sparse_allocate(int size){
 	int i;
 	blib_graph_sparse* g;
-	g=(blib_graph*)BLIB_MALLOC(sizeof(blib_graph));
+	g=(blib_graph_sparse*)BLIB_MALLOC(sizeof(blib_graph_sparse));
 	g->size=size;
-	g->edges=0;
 	g->alloced_edges=(int*)BLIB_MALLOC(sizeof(int)*size);
 	g->used_edges=(int*)BLIB_MALLOC(sizeof(int)*size);
-	g->adj=(blib_graph_sparse_edge**)BLIB_MALLOC(sizeof(blib_graph_sparse_edge*)*size);
+	g->adj=(int**)BLIB_MALLOC(sizeof(int*)*size);
 	for(i=0;i<size;i++){
-		g->adj[i]=(blib_graph_sparse_edge*)BLIB_MALLOC(sizeof(blib_graph_sparse_edge)*BLIB_GRAPH_SPARSE_MIN_ALLOC);
+		g->adj[i]=(int*)BLIB_MALLOC(sizeof(int)*BLIB_GRAPH_SPARSE_MIN_ALLOC);
 		g->used_edges[i]=0;
-		g->alloced_edges[i]=5;
+		g->alloced_edges[i]=BLIB_GRAPH_SPARSE_MIN_ALLOC;
 	}
 	return g;
 }
@@ -67,9 +49,9 @@ int blib_graph_edge(blib_graph_sparse* g,int a, int b){
 	if(a >= g->size || b >= g->size){
 		BLIB_ERROR("OUT OF BOUNDS graph access");
 	}
-	for(i=0;i<g->used_edges[a];9++){
-		if(g->adj[a][i].dest==b){
-			return g->adj[a][i].val;
+	for(i=0;i<g->used_edges[a];i++){
+		if(g->adj[a][i]==b){
+			return 1;
 		}
 	}
 	return BLIB_GRAPH_SPARSE_NO_EDGE_VAL;
@@ -80,24 +62,46 @@ int blib_graph_size(blib_graph_sparse* g){
 }
 
 void blib_graph_set_dir_edge( blib_graph_sparse* g, int a, int b,int val){
-	int *tmp,i,new_size;
+	int i,j,new_size;
+	int* new_list;
 	if(a >= g->size || b >= g->size){
 		BLIB_ERROR("OUT OF BOUNDS");
 	}
-	if(g->alloced_edges[a]==g->used_edges[i]){
-		new_size=(g->used_edges[a]*BLIB_GRAPH_SPARSE_EDGE_GROW+1);
-		tmp=(blib_graph_sparse_edge*)BLIB_MALLOC(sizeof(blib_graph_sparse_edge)*new_size);
-		for(i=0;i<g->used_edges;i++){
-			tmp[i].dest=g->arr[a][i].dest;
-			tmp[i].val=g->arr[a][i].val;
+	/*printf("Processing %d %d %d",a,b,val);*/
+	for(i=0;i<g->used_edges[a];i++){
+		if(g->adj[a][i]==b){
+			if(val!= BLIB_GRAPH_SPARSE_NO_EDGE_VAL)
+				return;
+			else{
+				
+				
+				for(j=i;j<g->used_edges[a]-1;j++){
+					g->adj[a][j]=g->adj[a][j+1];
+				}
+				g->used_edges[a]--;
+				
+				return;
+			}
 		}
-		free(g->arr[a]);
-		g->arr[a]=tmp;
-		g->alloced_edges[a]=new_size;
+		
 	}
-	g->arr[a][g->used_edges[a]].dest=b;
-	g->arr[a][g->used_edges[a]].val=val;
+	if(val==BLIB_GRAPH_SPARSE_NO_EDGE_VAL)
+		return;
+	if(g->alloced_edges[a]>g->used_edges[a]){
+		g->adj[a][g->used_edges[a]]=b;
+		g->used_edges[a]++;
+		return;
+	}
+	new_size=(g->alloced_edges[a]*BLIB_GRAPH_SPARSE_GROWTH_FACTOR+1);
+	new_list=(int*)BLIB_MALLOC(sizeof(int)*new_size);
+	for(i=0;i<g->used_edges[a];i++){
+		new_list[i]=g->adj[a][i];
+	}
+	new_list[g->used_edges[a]]=b;
+	free(g->adj[a]);
+	g->adj[a]=new_list;
 	g->used_edges[a]++;
+	g->alloced_edges[a]=new_size;
 }
 
 void blib_graph_set_edge(blib_graph_sparse* g, int a, int b, int val){
@@ -116,12 +120,12 @@ int blib_graph_is_edge(blib_graph_sparse* g, int a, int b){
 }
 
 /*Returns the address of the copy*/
-blib_graph_sparse* blib_graph_copy(blib_graph_sparse* a, blib_graph* b){
+blib_graph_sparse* blib_graph_copy(blib_graph_sparse* a, blib_graph_sparse* b){
 	int i,j;
 	if(b==NULL)
 		b=blib_graph_sparse_allocate(a->size);
 	if(b->size != a->size){
-		blib_graph_sparse_free(b);
+		blib_graph_free(b);
 		b=blib_graph_sparse_allocate(a->size);
 	}
 	for(i=0;i<a->size;i++){
@@ -131,17 +135,80 @@ blib_graph_sparse* blib_graph_copy(blib_graph_sparse* a, blib_graph* b){
 	return b;
 }
 
-int blib_graph_edge_count(blib_graph_sparse* a){
-	int i,j,edges;
-	edges=0;
-	/*dense version*/
-	for(i=0;i<blib_graph_size(a);i++){
-		for(j=i+1;j<blib_graph_size(a);j++){
-			edges++;
+
+
+void blib_graph_print_dot(blib_graph_sparse* g,FILE* stream,char** names){
+	int i,j;
+	fprintf(stream,"digraph g {\n");
+	if(names!=NULL){
+		for(i=0;i<blib_graph_size(g);i++){
+			if(names[i]!=NULL)
+				fprintf(stream,"v%d [label=\"%s\"];\n",i,names[i]);
 		}
 	}
-	return edges;
+	for(i=0;i<blib_graph_size(g);i++){
+		for(j=0;j<blib_graph_size(g);j++){
+			if(blib_graph_is_edge(g,i,j)){
+				fprintf(stream,"v%d -> v%d;\n",i,j);
+			}
+		}
+	}
+	fprintf(stream,"\n}\n");
 }
+
+
+
+#ifdef BLIB_UNIT_TEST
+
+int blib_graph_sparse_unit(void){
+	blib_graph_sparse* g;
+	int i,j,fail;
+	BLIB_ERROR("Starting Sparse Unit");
+	fail=0;
+	g=blib_graph_sparse_allocate(5);
+	if(g==NULL){
+		BLIB_ERROR("Graph didn't allocate");
+		fail=1;
+	}
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			if(blib_graph_is_edge(g,i,j)){
+				BLIB_ERROR("Graph not set to empty during init");
+				fail=1;
+			}
+		}
+	}
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			blib_graph_set_dir_edge(g,i,j,1);
+			if(!blib_graph_is_edge(g,i,j)){
+				BLIB_ERROR("Graph didn't allocate edge");
+				fail=1;
+			}
+		}
+	}
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			blib_graph_set_dir_edge(g,i,j,0);
+			if(blib_graph_is_edge(g,i,j)){
+				BLIB_ERROR("Graph didn't delete edge");
+				fail=1;
+			}
+		}
+	}
+	
+	
+	blib_graph_free(g);
+	/* Question how do we test to see if something is free?
+        if(g!=NULL){
+			BLIB_ERROR("Hey it didn't free the graph");
+        }*/
+	
+	BLIB_ERROR("Ending sparse unit");
+	return fail;
+}
+#endif
+
 
 
 #endif /*_BLIB_GRAPH_SPARSE_DEF_*/
